1bdc50303446bee78a5333acada9b686f18f36b4
Lostark13 <tushar9915@gmail.com>
2025-12-31 15:12:26 +0000

    sky: fixup soong bools

--- device.mk
+++ device.mk
@@ -103,7 +103,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl:64
@@ -253,7 +253,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)

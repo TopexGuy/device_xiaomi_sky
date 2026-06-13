4ddd209b0ccb20bc3893148340ec761877e1cd5e
TopexGuy <topexguy@gmail.com>
2026-06-13 04:04:34 +0000

    sky: LineageOS

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -23,8 +23,6 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
-BLISS_BUILDTYPE := OFFICIAL
-
 ## Device identifier
 PRODUCT_DEVICE := sky
 PRODUCT_NAME := lineage_sky

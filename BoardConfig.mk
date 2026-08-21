e790d2cfa46280e23527fca8b1695a2171f89262
TopexGuy <topexguy@gmail.com>
2026-08-21 22:10:17 +0530

    sky: vintf: add GNSS AIDL v1 to device framework matrix
    Vendor blob provides GNSS AIDL v1 but AOSP FCM only lists v2,
    causing checkvintf failures. Add device_framework_matrix.xml
    declaring IGnss AIDL v1 as optional for vendor compatibility.
    
    Signed-off-by: TopexGuy <topexguy@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -263,6 +263,7 @@ DEVICE_MANIFEST_RAVELIN_FILES := \
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
